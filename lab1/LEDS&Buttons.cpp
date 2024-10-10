ab1ef5c0239744ac7858498f6bf3c580a05daf87
kamel-mac <kamel12arag@gmail.com>
2024-10-10 19:08:12 +0300

    add comments

--- lab1/LEDS&Buttons.cpp
+++ lab1/LEDS&Buttons.cpp
@@ -1,21 +1,30 @@
 
+//b1..b4 buttons variables 
 int b1, b2, b3, b4; 
+//stat, u can consider it as an array[6] of contain binary values 
+//where each element of the array represents a LED (we have six LED)
 unsigned char stat = 0; 
+//it the number of shifts in the shift operation 
 int counter = 0; 
+//determines whether we have to decrement of increment the counter variable 
 bool mood = false; 
+//to save the stat of buttons variables 
 bool button_stat[4] ; 
 bool blanking_stat = true;
 
 void setup() {
+  //configure digital pins as inputs
   pinMode(4 , INPUT_PULLUP);
   pinMode(5 , INPUT_PULLUP);
   pinMode(6 , INPUT_PULLUP);
   pinMode(7 , INPUT_PULLUP);
-
+  
+  //activate PORTA, as output,
   DDRA = 0b11111111;
 }
 
 void loop() {
+  //read BTNs status
   b1 = digitalRead(4);
   b2 = digitalRead(5);
   b3 = digitalRead(6);
